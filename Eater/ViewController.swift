fca1c972268928008067054010d511df78d30a2f
LucasNguyen21 <llsogekingll@gmail.com>
2018-03-30 16:36:10 +1100

    Initial Commit

---
+++ Eater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eater
+//
+//  Created by Nguyen Dinh Thang on 30/3/18.
+//  Copyright © 2018 Nguyen Dinh Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
